100a1fc0fc15dd8bdf6dcd9380c0782e847fc501
Matt Williams <matt@milliams.com>
2022-05-09 18:38:53 +0100

    Add first version that parses args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::{thread, time};
+
+use argh::FromArgs;
+
+#[derive(FromArgs)]
+/// Calculate the value of the interesting simulation
+struct Args {
+    /// number of iterations
+    #[argh(positional)]
+    num: u8,
+}
+
+fn main() -> Result<(), String> {
+    let args: Args = argh::from_env();
+
+    let a = args.num;
+    println!("{:}", a);
+
+    let pause = time::Duration::from_secs(2);
+    thread::sleep(pause);
+
+    Ok(())
 }
